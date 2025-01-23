279a09864234e6ab1b1b401b8d57f3643a31525d
Jonas Kose <09-knurled-clogs@icloud.com>
2025-01-23 16:47:25 +0100

    trivial

--- lua/jonas/remap.lua
+++ lua/jonas/remap.lua
@@ -9,9 +9,16 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
+-- yank from clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- paste from clipboard
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>P", "\"+P")
+
 
 vim.keymap.set("n", "Q", "<noop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
